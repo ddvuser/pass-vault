a488e224d6b56bc172c74374fd2fd8994ffbad2f
Denys Doroshenko <denisdoroshenko99@gmail.com>
2023-10-29 17:08:02 +0200

    add forms and views

---
+++ PassVault/vault/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import RegisterForm, LoginForm
+
+def index(request):
+    return render(request, 'index.html')
+
+def user_register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form':form})
+
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(request, email=email, password=password)
+            if user:
+                login(request, user)
+                return redirect('index')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form':form})
+
+def user_logout(request):
+    return redirect('login')
