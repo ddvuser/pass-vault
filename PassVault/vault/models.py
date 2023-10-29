a2f2a41a8086fc71d72981b25baf5e2c8c5d1d58
Denys Doroshenko <denisdoroshenko99@gmail.com>
2023-10-29 16:26:01 +0200

    create initial models

--- PassVault/vault/models.py
+++ PassVault/vault/models.py
@@ -1,8 +1,23 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-class CustomUser(AbstractUser):
-    pass 
+from .managers import CustomUserManager
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_("email address"), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self) -> str:
+        return self.email
 
 class Folder(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
