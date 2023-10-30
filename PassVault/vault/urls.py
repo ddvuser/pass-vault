188b455e746034d82333b1683097d6757bb6f103
Denys Doroshenko <denisdoroshenko99@gmail.com>
2023-10-30 14:31:42 +0200

    add password change view

--- PassVault/vault/urls.py
+++ PassVault/vault/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register/', views.user_register, name='register'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
+    path('password/', views.change_password, name='change_password'),
 ]
--- PassVault/vault/views.py
+++ PassVault/vault/views.py
@@ -1,24 +1,29 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from .forms import RegisterForm, LoginForm
+from django.contrib import messages
+from django.contrib.auth.forms import PasswordChangeForm
 
 def index(request):
     return render(request, 'index.html')
 
 def user_register(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'You are successfully registered.')
             return redirect('login')
         else:
-            print(form.errors)   
+            error_messages = [error for field, error in form.errors.items()]
+            for error in error_messages:
+                messages.error(request, error) 
     else:
         form = RegisterForm()
     return render(request, 'register.html', {'form':form})
 
 def user_login(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
             email = form.cleaned_data['email']
@@ -26,11 +31,30 @@ def user_login(request):
             user = authenticate(request, email=email, password=password)
             if user:
                 login(request, user)
+                messages.success(request, 'You are logged in.')
                 return redirect('index')
+        messages.error(request, 'Wrong credentials!')
     else:
         form = LoginForm()
     return render(request, 'login.html', {'form':form})
 
 def user_logout(request):
     logout(request)
+    messages.success(request, 'You are logged out.')
     return redirect('login')
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, 'Your password was successfully updated.')
+            return redirect('index')
+        else:
+            error_messages = [error for field, error in form.errors.items()]
+            for error in error_messages:
+                messages.error(request, error)
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'change_password.html', {'form':form})
