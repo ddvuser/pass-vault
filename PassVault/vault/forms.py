3ed6e1c9541b01e39592934b3166511737176cd2
Denys Doroshenko <denisdoroshenko99@gmail.com>
2023-11-06 13:22:43 +0200

    add form widgets, change view_item design

--- PassVault/vault/forms.py
+++ PassVault/vault/forms.py
@@ -7,38 +7,54 @@ class RegisterForm(UserCreationForm):
         model = CustomUser
         fields = ['email', 'password1', 'password2']
 
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control'})
+        }
+
+
 class LoginForm(forms.Form):
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
 class AddItemForm(forms.ModelForm):
+    name = forms.CharField(max_length=80, strip=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(max_length=128, required=False, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(required=False, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    note = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 4}), required=False)
+    folder = forms.ModelChoiceField(queryset=Folder.objects.all(), required=False, widget=forms.Select(attrs={'class': 'form-select'}))
+
     class Meta:
         model = Entry
         fields = ['name', 'password', 'email', 'note', 'folder']
 
-    name = forms.CharField(max_length=80, strip=True)
-    password = forms.CharField(max_length=128, required=False, widget=forms.PasswordInput)
-    email = forms.EmailField(required=False)
-    note = forms.CharField(widget=forms.Textarea, required=False)
-    folder = forms.ModelChoiceField(queryset=Folder.objects.all(), required=False)
 
 class EditItemForm(forms.ModelForm):
     class Meta:
         model = Entry
         fields = ['name', 'password', 'email', 'note', 'folder']
 
-    widgets = {
-        'note': forms.Textarea(attrs={'rows': 4}),
-    }
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'password': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'note': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+            'folder': forms.Select(attrs={'class': 'form-select'}),
+        }
 
 class AddFolderForm(forms.ModelForm):
     class Meta:
         model = Folder
         fields = ['name',]
     
-    name = forms.CharField(max_length=80, strip=True)
+    name = forms.CharField(max_length=80, strip=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
 
 class EditFolderForm(forms.ModelForm):
     class Meta:
         model = Entry
         fields = ['name',]
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+        }
